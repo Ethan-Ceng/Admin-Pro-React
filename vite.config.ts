38db1e51f4c3e20a84b0c2da935ee444eea671a5
Ethan <CengFuming@qq.com>
2024-06-29 17:33:33 +0800

    feat: 基础配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, '../src'),
+      },
+      {
+        find: 'assets',
+        replacement: resolve(__dirname, '../src/assets'),
+      }
+    ],
+    extensions: ['.ts', '.js'],
+  },
   plugins: [react()],
 })
